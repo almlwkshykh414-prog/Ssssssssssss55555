2fe9a3c772905c88946f3c43a6e74f808bc14b1f
almlwkshykh414-prog <almlwkshykh414@gmail.com>
2026-03-27 11:35:21 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "8786516305:AAELBV_f0F2XYP6N67apDq-9xOq0ip3zUIU",
   id:"8128490926",
-  address:"Enter your url"
+  address:"https://ssssssssssss55555.onrender.com"
 };
